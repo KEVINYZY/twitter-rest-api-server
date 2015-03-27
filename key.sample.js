34d52d05191989a67b6946420773c1599510fabf
Ruan Yifeng <yifeng.ruan@gmail.com>
2015-03-27 21:18:14 +0800

    add key.js

---
+++ key.sample.js
@@ -0,0 +1,9 @@
+// put your key information here,
+// and rename the file into key.js
+
+module.exports = {
+  consumer_key: '...',
+  consumer_secret: '...',
+  access_token: '...',
+  access_token_secret: '...'
+}
--- twitter.js
+++ twitter.js
@@ -1,20 +1,21 @@
 var Twit   = require('twit'),
     async  = require('async'),
     moment = require('moment'),
+    key = require('key'),
     T      = new Twit({
-      consumer_key: process.env.TwitterConsumerKey || '...',
-      consumer_secret: process.env.TwitterConsumerSecret || '...',
-      access_token: process.env.TwitterAccessToken || '...',
-      access_token_secret: process.env.TwitterAccessTokenSecret || '...'
+      consumer_key: process.env.TwitterConsumerKey || key.consumer_key,
+      consumer_secret: process.env.TwitterConsumerSecret || key.consumer_secret,
+      access_token: process.env.TwitterAccessToken || key.access_token,
+      access_token_secret: process.env.TwitterAccessTokenSecret || key.access_token_secret
     }),
-    
+
     mapReducingTweets = function(tweet, callback) {
       callback(null, simplify(tweet));
     },
 
     simplify = function(tweet) {
       var date = moment(tweet.created_at, "ddd MMM DD HH:mm:ss zz YYYY");
-      date.lang( process.env.MomentLang );
+      date.lang( process.env.MomentLang || 'zh-CN' );
       return {
         date: date.format('MMMM Do YYYY, h:mm:ss a'),
         id: tweet.id,
@@ -34,5 +35,5 @@ module.exports = function(username, callback) {
     else async.map(tweets, mapReducingTweets, function(err, simplified_tweets) {
       callback(null, simplified_tweets);
     });
-  })
+  });
 };
