affa1e6b1b1ef637c7263b79a1faf3175d9b8596
Ruan Yifeng <yifeng.ruan@gmail.com>
2015-03-28 06:50:07 +0800

    edit typos

--- key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'RC9keKHYXeedFHpQHz1KQfLqG',
-  consumer_secret: 'dycsYaAmBF3bq0ZMET5WZ14t0oW5DQPn2JEFXBrxR3IN1vuSjG',
-  access_token: '1580781-T1l532MxYhazx2uL6XVmvewwLF9y8qLey47echYRFT',
-  access_token_secret: 'szJHNJQ903JcUNWNTJq8Yh9kk8JLDI3M46BcStj3j5SBe'
-}
